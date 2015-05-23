c98d153e38d7f1f6503f7857b4e9dae9d663152f
0xbadfca11 <0xbadfca11@gmail.com>
2015-05-23 21:37:31 +0900

    cleaning

--- wof_compact.cpp
+++ wof_compact.cpp
@@ -1,18 +1,17 @@
 #include <windows.h>
 #include <winioctl.h>
 
-int __cdecl wmain( int argc, PWSTR argv[] )
+struct WOF
+{
+	WOF_EXTERNAL_INFO wof_header = { WOF_CURRENT_VERSION, WOF_PROVIDER_FILE };
+	FILE_PROVIDER_EXTERNAL_INFO provider = { FILE_PROVIDER_CURRENT_VERSION, FILE_PROVIDER_COMPRESSION_XPRESS4K };
+};
+static_assert( sizeof WOF::wof_header == offsetof( WOF, provider ), "" );
+
+int __cdecl wmain( int, PWSTR argv[] )
 {
 	HANDLE f = CreateFileW( argv[1], GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr );
-	struct
-	{
-		WOF_EXTERNAL_INFO wof;
-		FILE_PROVIDER_EXTERNAL_INFO provider;
-	} compress{
-		{ WOF_CURRENT_VERSION, WOF_PROVIDER_FILE },
-		{ FILE_PROVIDER_CURRENT_VERSION, FILE_PROVIDER_COMPRESSION_XPRESS4K, 0 }
-	};
-	static_assert( sizeof compress.wof == offsetof( decltype( compress ), provider ), "" );
+	WOF compress;
 	ULONG dummy;
 	return !DeviceIoControl( f, FSCTL_SET_EXTERNAL_BACKING, &compress, sizeof compress, nullptr, 0, &dummy, nullptr );
 }
