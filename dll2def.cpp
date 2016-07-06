4911cdb7944657242c6531d2217c954117fe0d20
0xbadfca11 <0xbadfca11@gmail.com>
2016-07-06 22:52:01 +0900

    Bugfix dll2def.cpp

--- dll2def.cpp
+++ dll2def.cpp
@@ -4,6 +4,7 @@
 #include <shlwapi.h>
 #include <cstdio>
 #include <cstring>
+#include <memory>
 #pragma comment(lib, "dbghelp")
 #pragma comment(lib, "shlwapi")
 
@@ -11,16 +12,24 @@ int __cdecl wmain(int argc, PWSTR argv[])
 {
 	while (--argc)
 	{
-		auto base_address = reinterpret_cast<PCHAR>(LoadLibraryExW(argv[argc], nullptr, LOAD_LIBRARY_AS_DATAFILE));
+		HANDLE h = CreateFileW(argv[argc], FILE_GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
+		if (h == INVALID_HANDLE_VALUE)
+			continue;
+		HANDLE m = CreateFileMappingW(h, nullptr, PAGE_READONLY | SEC_IMAGE_NO_EXECUTE, 0, 0, nullptr);
+		CloseHandle(h);
+		if (!m)
+			continue;
+		std::shared_ptr<char> base_address(reinterpret_cast<char*>(MapViewOfFile(m, FILE_MAP_READ, 0, 0, 0)), UnmapViewOfFile);
+		CloseHandle(m);
 		if (!base_address)
 			continue;
 		ULONG Size;
-		auto export_directory = static_cast<PIMAGE_EXPORT_DIRECTORY>(ImageDirectoryEntryToDataEx(base_address, TRUE, IMAGE_DIRECTORY_ENTRY_EXPORT, &Size, NULL));
+		auto export_directory = static_cast<PIMAGE_EXPORT_DIRECTORY>(ImageDirectoryEntryToDataEx(base_address.get(), TRUE, IMAGE_DIRECTORY_ENTRY_EXPORT, &Size, NULL));
 		if (!export_directory)
 			continue;
 		if (!export_directory->AddressOfNames)
 			continue;
-		auto AddressOfNames = reinterpret_cast<PULONG>(base_address + export_directory->AddressOfNames);
+		auto AddressOfNames = reinterpret_cast<PULONG>(base_address.get() + export_directory->AddressOfNames);
 		auto libfilename = PathFindFileNameW(argv[argc]);
 		WCHAR deffilename[MAX_PATH];
 		if (wcscpy_s(deffilename, libfilename))
@@ -31,7 +40,7 @@ int __cdecl wmain(int argc, PWSTR argv[])
 			continue;
 		fputs("EXPORTS\n", deffile);
 		for (decltype(IMAGE_EXPORT_DIRECTORY::NumberOfNames) i = 0; i < export_directory->NumberOfNames; ++i)
-			fprintf(deffile, "\t%s\n", base_address + AddressOfNames[i]);
+			fprintf(deffile, "\t%s\n", base_address.get() + AddressOfNames[i]);
 		fclose(deffile);
 	}
 }
